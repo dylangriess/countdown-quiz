641ee93a75fe8c1ab9e5a7d69a6b67fc696a7634
dylangriess <dylangriess@gmail.com>
2022-08-10 21:01:11 -0600

    restructured questions to be in java element, simplified HTML element so all questions were not visible, restructured CSS styles

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,96 @@
-console.log("Hello");
+var startButton = document.querySelector("#start-button")
+
+function startGame();
+
+
+
+function nextQuestion ();
+
+
+
+function selectAnswer();
+//
+// <div class="container" id="visible">
+// <h2>Do you want to begin?</h2>
+/* <p>Click below to start.</p>
+<button class="button" id="begin-button">Begin</button>
+</div>
+
+<!--Question1-->
+<div class="container" id="hidden">
+<h2>
+    1) Which of the following functions of an Array object joins all elements of an array into a string?
+</h2>
+<div id="answer-buttons">
+    <button class="button" id="incorrect">concat()</button>
+    <button class="button" id="correct">join()</button>
+    <button class="button" id="incorrect">pop()</button>
+    <button class="button" id="incorrect">map()</button>
+</div>
+</div>
+
+<!--Question2-->
+<div class="container" id="hidden">
+<h2>
+    2)The "function" and " var" are known as:
+</h2>
+<div id="answer-buttons">
+    <button class="button" id="incorrect">Keywords</button>
+    <button class="button" id="incorrect">Data Types</button>
+    <button class="button" id="incorrect">Prototypes</button>
+    <button class="button" id="correct">Declaration Statements</button>
+</div>
+</div>
+
+<!--Question3-->
+<div class="container" id="hidden">
+<h2>
+  3) Which of the following variables takes precedence over the others if the names are the same?
+</h2>
+<div id="answer-buttons">
+    <button class="button" id="incorrect">Global variable</button>
+    <button class="button" id="correct">Local Element</button>
+    <button class="button" id="incorrect">Both Of The Above</button>
+    <button class="button" id="incorrect">None Of The Above</button>
+</div>
+</div>
+
+<!--Question4-->
+<div class="container" id="hidden">
+<h2>
+  4) In JavaScript the x===y statement implies that:
+</h2>
+<div id="answer-buttons">
+    <button class="button" id="incorrect">Both x and y are equal in value, type and reference address as well.</button>
+    <button class="button" id="incorrect">Both are x and y are equal in value only.</button>
+    <button class="button" id="incorrect">Both are not the same at all.</button>
+    <button class="button" id="correct">Both are equal in the value and data type.</button>
+</div>
+</div>
+
+<!--Question5-->
+<div class="container" id="hidden">
+<h2>
+  5) Which one of the following symbol is used for creating comments in the javascript:
+</h2>
+<div id="answer-buttons">
+    <button class="button" id="incorrect">\\</button>
+    <button class="button" id="correct">//</button>
+    <button class="button" id="incorrect">\* *\</button>
+//     <button class="button" id="incorrect">\/</button>
+// </div>
+// </div>
+
+// <!--Question6-->
+// <div class="container" id="hidden">
+// <h2>
+//   6) A collection of elements of the same data type which may either in order or not, is called _____.
+// </h2>
+// <div id="answer-buttons">
+//     <button class="button" id="incorrect">String</button>
+//     <button class="button" id="incorrect">Serialized Object</button>
+//     <button class="button" id="incorrect">Parameter</button>
+//     <button class="button" id="correct">Array</button>
+// </div>
+// </div>
+// <!--Questions credited to https://www.tutorialspoint.com/javascript/javascript_online_quiz.htm; https://www.javatpoint.com/javascript-mcq-->;*/
